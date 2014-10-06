022998b4b7f01cfb858b5535c7522544491ae0b8
Félix Saparelli <felix@passcod.name>
2014-10-06 14:19:46 +1300

    Add #on_update to Variable

--- lib/variable.rb
+++ lib/variable.rb
@@ -3,10 +3,18 @@ class Sebastian::Variable
   
   def initialize(&block)
     @block = block
+    @callbacks = []
     update
   end
 
   def update
     @result = @block.call
+    @callbacks.each do |callback|
+      callback.call(@result)
+    end    
+  end
+
+  def on_update(&block)
+    @callbacks.push block
   end
 end
